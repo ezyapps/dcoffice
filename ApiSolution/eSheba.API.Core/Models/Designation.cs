acbfb9cc50207c76d771ff13be2e724b0b33f21d
Md. Anisur Rahman <engr.nayan@gmail.com>
2020-11-10 11:09:11 +0600

    API project removed

--- ApiSolution/eSheba.API.Core/Models/Designation.cs
+++
@@ -1,9 +0,0 @@
-using eSheba.API.Shared.Models;
-
-namespace eSheba.API.Core.Models
-{
-    public class Designation: Entity
-    {
-        public string Title { get; set; }
-    }
-}
--- eSheba.SPA/src/app/modules/setup/models/right.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../../user/models/user';
-
-export interface Right {
-  id: number;
-  code: string;
-  name: string;
-  createdby: User;
-  datecreated: Date;
-}
